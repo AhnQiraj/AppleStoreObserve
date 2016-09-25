d63ecbb94cadfad0c5e836da952938744253a2dd
AhnQiraj <m@liuyang.io>
2016-09-25 15:03:23 +0900

    First commit

---
+++ Iphone/StoreModel.h
@@ -0,0 +1,13 @@
+//
+//  StoreModel.h
+//  Iphone
+//
+//  Created by 刘洋 on 16/9/18.
+//  Copyright © 2016年 ddicar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StoreModel : NSObject
+
+@end
---
+++ Iphone/StoreModel.m
@@ -0,0 +1,13 @@
+//
+//  StoreModel.m
+//  Iphone
+//
+//  Created by 刘洋 on 16/9/18.
+//  Copyright © 2016年 ddicar. All rights reserved.
+//
+
+#import "StoreModel.h"
+
+@implementation StoreModel
+
+@end
